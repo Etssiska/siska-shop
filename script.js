fec72693a1333169b9bd9790c46d642c680075fe
Lexxy472 <adhiviajulian@gmail.com>
2022-05-11 10:24:33 +0700

    fixed bug

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const menuToggle = document.querySelector(".menu-toggle input");
 const nav = document.querySelector("nav ul");
+const menu = document.getElementById("menu");
 
 menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
+  menu.classListt.toggle("d-none");
 });
